fa69bc56ea417bd50beba5e5ad3a1fd118d37aa1
Abdus Sadik Molla <abdussadikmolla@gmail.com>
2023-01-05 10:55:24 +0530

    deleted

--- area_of_circle.c
+++
@@ -1,9 +0,0 @@
-// Area of the circle
-#include<stdio.h>
-int main(){
-    float radius ;
-    printf("Enter radius of the circle: ");
-    scanf("%f",&radius);
-    printf("Area of the circle: %.2f",3.14*radius*radius);
-    return 0;
-}
--- area_of_square.c
+++
@@ -1,9 +0,0 @@
-// write a c program to calculate the area of the square
-#include<stdio.h>
-int main(){
-    float side;
-    printf("Enter the side of the square: ");
-    scanf("%f",&side);
-    printf("Area of the square is = %.2f unit.",side*side);
-    return 0;
-}
